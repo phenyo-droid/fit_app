6bc19a20cbf2afc6881d39fd0a3040bcb0a019f1
phenyo-droid <pnkoana61@gmail.com>
2024-05-09 07:01:21 +0200

    fonts
    added fonts

---
+++ lib/pages/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatlessWidget{
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appbar: AppBar(),
+    )
+  }
+}
+
